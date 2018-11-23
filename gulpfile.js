054565202943d04b6d22507abc5cb601b0acb4f9
christian-gss <christianmarquez.gss@gmail.com>
2018-11-23 09:48:15 +0800

    #1 Fix gulpfile for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,111 +1,253 @@
-//initialize all of our variables
-var app, base, concat, directory, gulp, gutil, hostname, path, refresh, sass, uglify, imagemin, minifyCSS, del, browserSync, autoprefixer, gulpSequence, shell, sourceMaps, plumber;
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sassGlob = require("gulp-sass-glob"),
+    cssImport = require('postcss-import'),
+    autoprefixer = require('autoprefixer'),
+    frontnote = require('gulp-frontnote'),
+    uglify = require('gulp-uglify'),
+    browserify = require('browserify'),
+    babelify = require('babelify');
+    source = require('vinyl-source-stream');
+    buffer = require('vinyl-buffer');
+    watchify = require('watchify');
+    rimraf = require('rimraf'),
+    runSequence = require('run-sequence'),
+    browserSync = require('browser-sync'),
+    ssi = require('browsersync-ssi'),
+    plumber = require("gulp-plumber"),
+    htmlv = require('gulp-htmlhint'),
+    notify = require('gulp-notify'),
+    postcss = require('gulp-postcss'),
+    csssort = require('css-declaration-sorter'),
+    mqpacker = require("css-mqpacker"),
+    cssnano = require("cssnano"),
+    sourcemaps = require('gulp-sourcemaps'),
+    syntax_scss = require('postcss-scss'),
+    stylelint = require('stylelint');
+    reporter = require('postcss-reporter'),
+    changed  = require('gulp-changed'),
+    imagemin = require('gulp-imagemin'),
+    imageminPngquant = require('imagemin-pngquant'),
+    cache = require('gulp-cached'),
+    rename = require('gulp-rename'),
+    cleanCSS = require('gulp-clean-css');
 
-var autoPrefixBrowserList = ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'];
+/* developディレクトリ */
+var src = {
+  'root': 'src/',
+  'html': 'src/.html',
+  'css': ['src/scss/*.scss', 'src/scss/**/*.scss'],
+  // 'js': ['src/js/**/*.js', '!js/min/**/*.js'],
+  'js': 'src/js/script.js',
+  'image': 'src/images/**/*.+(jpg|jpeg|png|gif|svg|ico)',
+  'imageWatch': 'src/images/**/*',
+  'public': 'public/**/*'
+};
 
-//load all of our dependencies
-//add more here if you want to include more libraries
-gulp        = require('gulp');
-gutil       = require('gulp-util');
-concat      = require('gulp-concat');
-uglify      = require('gulp-uglify');
-sass        = require('gulp-sass');
-sassGlob    = require("gulp-sass-glob"),
-sourceMaps  = require('gulp-sourcemaps');
-imagemin    = require('gulp-imagemin');
-minifyCSS   = require('gulp-minify-css');
-notify      = require('gulp-notify');
-browserify = require('browserify'),
-wait        = require('gulp-wait');
-browserSync = require('browser-sync');
-autoprefixer = require('gulp-autoprefixer');
-gulpSequence = require('gulp-sequence').use(gulp);
-shell       = require('gulp-shell');
-plumber     = require('gulp-plumber');
+/* tmpディレクトリ */
+var tmp = {
+  'root': 'tmp/',
+  'image': 'tmp/images/',
+  'css': 'tmp/css/',
+  'js': 'tmp/js/'
+};
 
-gulp.task('browserSync', function() {
-    browserSync({
-        server: {
-            baseDir: "./src/"
-        },
-    });
+/* publicディレクトリ */
+var public = {
+  'root': 'public/',
+  'html': 'public/**/*.html',
+  'image': 'public/images/',
+  'css': 'public/css/',
+  'js': 'public/js/'
+};
+
+
+gulp.task('default', ['clean:tmp'], function() {
+  runSequence(
+    'watch',
+    'server'
+  )
 });
 
+// /**
+//  * Gulpの処理を通さないディレクトリです。
+//  * テスト用のディレクトリにコピーします。
+//  */
+// gulp.task('public', function() {
+//   return gulp.src(src.public)
+//   .pipe(gulp.dest(tmp.root));
+// });
+
+/**
+ * 出力用のディレクトリを削除します。
+ */
+gulp.task('clean:tmp', function (cb) {
+  return rimraf(tmp.root, cb);
+});
+
+gulp.task('build', function(callback) {
+  runSequence(
+    ['html', 'check-html', 'css', 'imagemin', 'js'], callback
+  )
+});
+
+gulp.task('server', function(){
+  browserSync.init({
+    server: {
+      baseDir: tmp.root,
+      middleware:[
+        ssi({
+          ext: '.html',
+          baseDir: tmp.root
+        })
+      ]
+    }
+  });
+});
+
+/**
+ * /public/以下のHTMLファイルを監視、更新があれば反映します。
+ */
 gulp.task('html', function() {
-    //watch any and all HTML files and refresh when something changes
-    return gulp.src('*.html')
-        .pipe(plumber())
-        .pipe(browserSync.reload({stream: true}));s
+  return gulp.src(public.html)
+  .pipe(browserSync.reload({stream: true}));
 });
 
-//compressing images & handle SVG files
-gulp.task('images', function(tmp) {
-    gulp.src(['src/images/*.jpg', 'src/images/*.png', 'src/images/**/*.jpg', 'src/images/**/*.png'])
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
-        .pipe(gulp.dest('src/images'));
+// htmllint
+gulp.task('check-html', function(){
+  return gulp.src('src/*.html')
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>;")
+    }))
+    .pipe(cache('html'))
+    .pipe(htmlv())
+    .pipe(htmlv.reporter())
+    .pipe(gulp.dest(tmp.root))
+    .pipe(gulp.dest(public.root))
 });
 
-//compiling our SCSS files
-gulp.task('styles', function() {
-    //the initializer / master SCSS file, which will just be a file that imports everything
-    return gulp.src('src/scss/common.scss')
-                .pipe(wait(200))
-                //prevent pipe breaking caused by errors from gulp plugins
-                .pipe(plumber({
-                  errorHandler: notify.onError("Error: <%= error.message %>;")
-                }))
-                //get sourceMaps ready
-                .pipe(sourceMaps.init())
-                .pipe(sassGlob())
-                //include SCSS and list every "include" folder
-                .pipe(sass({
-                      errLogToConsole: true,
-                      includePaths: [
-                          'src/scss/'
-                      ]
-                }))
-                .pipe(autoprefixer({
-                   browsers: autoPrefixBrowserList,
-                   cascade:  true
-                }))
-                //catch errors
-                .on('error', gutil.log)
-                //the final filename of our combined css file
-                .pipe(concat('style.css'))
-                //get our sources via sourceMaps
-                .pipe(sourceMaps.write())
-                //where to save our final, compressed css file
-                .pipe(gulp.dest('src/css'))
-                .pipe(browserSync.reload({stream: true, once: true}));
+gulp.task('sass', function(){
+  return gulp.src(src.css)
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>;")
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(frontnote({
+      out: './_styleguide',
+      css: '../tmp/css/common.css'
+    }))
+    .pipe(sassGlob())
+    .pipe(postcss([
+      stylelint(),
+      reporter({clearMessages: true, throwError: true})
+    ], {syntax: syntax_scss}))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(postcss([
+      autoprefixer({
+        browsers: ['last 2 version'],
+        grid: true
+      }),
+      csssort({
+        order: 'smacss'
+      }),
+      cssImport({path: ['node_modules']}),
+      mqpacker()
+      // cssnano({ autoprefixer: false })
+    ]))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(tmp.css))
+    .pipe(browserSync.reload({stream: true, once: true}))
 });
 
-
-var browserify = require('browserify'),
-    gulp       = require('gulp'),
-    source     = require('vinyl-source-stream'),
-    buffer     = require('vinyl-buffer'),
-    uglify     = require('gulp-uglify');
-
-gulp.task('scripts', function () {
-    return browserify([__dirname + '/src/js/main.js']).bundle()
-        .pipe(source('src/js'))
-        .pipe(buffer())
-        .pipe(uglify())
-        .pipe(concat('bundle.js'))
-        .pipe(gulp.dest('src/js'));
+// css圧縮
+gulp.task('css', ['sass'], function(){
+  return gulp.src('tmp/css/common.css')
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>;")
+    }))
+    .pipe(postcss([
+      cssnano({ autoprefixer: false })
+    ]))
+    .pipe(gulp.dest(public.css))
 });
 
-//this is our master task when you run `gulp` in CLI / Terminal
-//this is the main watcher to use when in active development
-//  this will:
-//  startup the web server,
-//  start up browserSync
-//  compress all scripts and SCSS files
-gulp.task('default', ['browserSync','styles','scripts'], function() {
-    //a list of watchers, so it will watch all of the following files waiting for changes
-    gulp.watch('src/scss/**', ['styles']);
-    gulp.watch('src/js/**', ['scripts']);
-    gulp.watch('images/**', ['images']);
+/* ES2015以降のコードをES5に変換（トランスコンパイル）します。 */
+function bundle(watching = false) {
+  const b = browserify({
+    entries: src.js,
+    transform: ['babelify'],
+    debug: true,
+    plugin: (watching) ? [watchify] : null
+  })
+  .on('update', function() {
+    bundler();
+    console.log('scripts rebuild');
+  });
+
+  function bundler() {
+    return b.bundle()
+      .on('error', function(err) {
+        console.log(err.message);
+      })
+      .pipe(source('script.js'))
+      .pipe(gulp.dest(tmp.js))
+      // 生のデータをストリームにし、vinylのオブジェクト/ファイルに変換
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(uglify({output: {comments: /^!/}}))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(public.js))
+      .pipe(browserSync.reload({stream: true, once: true}))
+  }
+  return bundler();
+}
+
+gulp.task('js', function() {
+  bundle();
+});
+
+// js圧縮
+// gulp.task('js', function(){
+//   pump([
+//       plumber({
+//         errorHandler:notify.onError('Error: <%= error.message %>')
+//       }),
+//       gulp.src(src.js),
+//       gulp.dest(tmp.js),
+//       uglify(),
+//       gulp.dest(public.js)
+//     ]
+//   );
+//   return gulp.src('public/js/**/*.js')
+//     .pipe(browserSync.reload({stream: true, once: true}))
+// });
+
+gulp.task('imagemin', function(){
+  var imageminOptions = {
+        optimizationLevel: 7,
+        progressive: true,
+        interlaced: true,
+        use:imageminPngquant({quality: '65-80',speed: 1})
+      };
+
+  return gulp.src(src.image)
+    .pipe(changed(tmp.image))
+    .pipe(plumber({
+      errorHandler:notify.onError('Error: <%= error.message %>')
+    }))
+    .pipe(imagemin(imageminOptions))
+    .pipe(gulp.dest(tmp.image))
+    .pipe(gulp.dest(public.image))
+    .pipe(browserSync.reload({stream: true, once: true}));
+});
+
+gulp.task('watch', ['build'], function(){
+  gulp.watch(src.html,['check-html']);
+  gulp.watch(public.html,['html']);
+  gulp.watch(src.js,['js']);
+  gulp.watch(src.css,['css']);
+  gulp.watch(src.image, ['imagemin']);
+  // gulp.watch(src.public, ['public']);
 });
